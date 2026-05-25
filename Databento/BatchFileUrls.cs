c6d473c0ab30f7dbdb485bb698550c81eb4a0231
Louis S. Berman <louis@squideyes.com>
2026-05-25 09:38:11 -0400

    Successfully fetched ES,NQ,GC,CL

--- Databento/BatchFileUrls.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace StbaFetcher;
-
-internal sealed record BatchFileUrls
-{
-    [JsonPropertyName("https")] public string? Https { get; init; }
-    [JsonPropertyName("ftp")] public string? Ftp { get; init; }
-}
